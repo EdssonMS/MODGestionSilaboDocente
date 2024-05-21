7cba688d8b5933a2648241ccd23f5b8fdc6929fc
Seraycrep <percy.rojas8@unmsm.edu.pe>
2024-05-20 19:51:35 -0500

    commit prueba

--- src/main/java/com/grupo/MODGestionSilaboDocente/ModGestionSilaboDocenteApplication.java
+++ src/main/java/com/grupo/MODGestionSilaboDocente/ModGestionSilaboDocenteApplication.java
@@ -8,7 +8,7 @@ public class ModGestionSilaboDocenteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModGestionSilaboDocenteApplication.class, args);
-		System.out.println("HOLA MUNDO");
+		System.out.println("Hola mundo");
 	}
 
 }
